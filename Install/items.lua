be624f1908dc537f14a7566c717e4411f7f68494
Lation <lationj@pm.me>
2023-06-27 17:18:00 -0400

    Forgot install folder (oops)

---
+++ Install/items.lua
@@ -0,0 +1,104 @@
+-- Add these items into your ox_inventory/data/items.lua file
+
+['pickaxe'] = {
+    label = 'Pickaxe',
+    weight = 325,
+    stack = true,
+    degrade = 30 -- This is the time in minutes until a pickaxe degrades and is removed from inventory
+},
+
+['scrap_metal'] = {
+    label = 'Scrap Metal',
+    weight = 280,
+    stack = true,
+},
+
+['stone'] = {
+    label = 'Stone',
+    weight = 265,
+    stack = true,
+},
+
+['raw_copper'] = {
+    label = 'Raw Copper',
+    weight = 245,
+    stack = true,
+},
+
+['raw_iron'] = {
+    label = 'Raw Iron',
+    weight = 225,
+    stack = true,
+},
+
+['raw_steel'] = {
+    label = 'Raw Steel',
+    weight = 210,
+    stack = true,
+},
+
+['raw_silver'] = {
+    label = 'Raw Silver',
+    weight = 190,
+    stack = true,
+},
+
+['raw_gold'] = {
+    label = 'Raw Gold',
+    weight = 190,
+    stack = true,
+},
+
+['raw_diamond'] = {
+    label = 'Raw Diamond',
+    weight = 165,
+    stack = true,
+},
+
+['raw_emerald'] = {
+    label = 'Raw Emerald',
+    weight = 140,
+    stack = true,
+},
+
+['copper'] = {
+    label = 'Copper',
+    weight = 225,
+    stack = true,
+},
+
+['iron'] = {
+    label = 'Iron',
+    weight = 200,
+    stack = true,
+},
+
+['steel'] = {
+    label = 'Steel',
+    weight = 190,
+    stack = true,
+},
+
+['silver'] = {
+    label = 'Silver',
+    weight = 175,
+    stack = true,
+},
+
+['gold'] = {
+    label = 'Gold',
+    weight = 155,
+    stack = true,
+},
+
+['diamond'] = {
+    label = 'Diamond',
+    weight = 140,
+    stack = true,
+},
+
+['emerald'] = {
+    label = 'Emerald',
+    weight = 125,
+    stack = true,
+},
