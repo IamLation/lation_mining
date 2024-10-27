c0d969bc5f2b8bc71237c4382191cee67c64d40f
AlphaDevelopment <109308073+bylickilabs@users.noreply.github.com>
2024-10-27 13:43:30 +0100

    Create Items.sql
    Add Items SQL

---
+++ Items.sql
@@ -0,0 +1,18 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('pickaxe', 'Spitzhacke', 1, 0, 1),
+	('copper', 'Kupfer', 1, 0, 1),
+	('diamond', 'Diamant', 1, 0, 1),
+	('emerald', 'Smaragd', 1, 0, 1),
+	('gold', 'Gold', 1, 0, 1),
+	('iron', 'Eisen', 1, 0, 1),
+	('silver', 'Silber', 1, 0, 1),
+	('steel', 'Stahl', 1, 0, 1),
+	('stone', 'Stein', 1, 0, 1),
+	('scrap_metal', 'Metallschrott', 1, 0, 1),
+	('raw_copper', 'Rohkupfer', 1, 0, 1),
+	('raw_iron', 'Roheisen', 1, 0, 1),
+	('raw_steel', 'Rohstahl', 1, 0, 1),
+	('raw_silver', 'Rohsilber', 1, 0, 1),
+	('raw_gold', 'Rohgold', 1, 0, 1),
+	('raw_diamond', 'Rohdiamant', 1, 0, 1),
+	('raw_emerald', 'Rohsmaragd', 1, 0, 1);
